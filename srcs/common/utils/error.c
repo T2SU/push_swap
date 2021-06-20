c867c7588a7e2a26fae4ccec8f86d2eaf4118f3c
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-06-21 06:08:31 +0900

    remove error description

--- srcs/common/utils/error.c
+++ srcs/common/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 01:31:40 by smun              #+#    #+#             */
-/*   Updated: 2021/06/21 05:33:21 by smun             ###   ########.fr       */
+/*   Updated: 2021/06/21 06:08:18 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,6 @@ void		raise_error_description(const char *des, const char *desc)
 	print_error("Error\n");
 	(void)des;
 	(void)desc;
-	if (des != NULL)
-	{
-		print_error(des);
-		if (desc != NULL)
-		{
-			print_error(": ");
-			print_error(desc);
-		}
-		print_error("\n");
-	}
 	exit(EXIT_FAILURE);
 }
 
