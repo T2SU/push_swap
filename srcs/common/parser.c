c425caa3a42a159190e0df6c0ed26b1c43f55990
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-06-27 16:16:08 +0900

    add parsing for zsh, sh arguments

--- srcs/common/parser.c
+++ srcs/common/parser.c
@@ -6,11 +6,50 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 19:43:41 by smun              #+#    #+#             */
-/*   Updated: 2021/06/19 03:16:04 by smun             ###   ########.fr       */
+/*   Updated: 2021/06/27 16:10:44 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
+#include <stdlib.h>
+
+static void		parse_number(t_list *list, char *arg, int index, int length)
+{
+	char		*tmp;
+	int			num;
+
+	if (NULL == (tmp = malloc(length + 1)))
+		raise_error("out of memory");
+	ft_memcpy(tmp, &arg[index], length);
+	tmp[length] = '\0';
+	if (!ft_atoi_strict(tmp, &num))
+		raise_error_description("unparsable into integer.", tmp);
+	list_push_one(list, num);
+	free(tmp);
+}
+
+static void		parse_element(t_list *list, char *arg)
+{
+	int			i;
+	int			index;
+	int			length;
+
+	i = 0;
+	index = 0;
+	length = 0;
+	while (TRUE)
+	{
+		if (arg[i] == ' ' || arg[i] == '\0')
+		{
+			parse_number(list, &arg[i], index, length);
+			index += length + 1;
+			length = 0;
+		}
+		if (arg[i++] == '\0')
+			break ;
+		length++;
+	}
+}
 
 void			parse_into_list(t_list *list, int argc, char *argv[])
 {
@@ -22,10 +61,5 @@ void			parse_into_list(t_list *list, int argc, char *argv[])
 		raise_error("lacked arguments.");
 	i = argc - 1;
 	while (i >= start_index)
-	{
-		if (!ft_atoi_strict(argv[i], &num))
-			raise_error_description("unparsable into integer.", argv[i]);
-		list_push_one(list, num);
-		i--;
-	}
+		parse_element(list, argv[i--]);
 }
