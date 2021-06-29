b3e8f6728e2c3417501aa42d9a627fdd74f8a8a1
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-06-29 19:18:28 +0900

    ddddd

--- srcs/common/libft/ft_itoa.c
+++ srcs/common/libft/ft_itoa.c
@@ -6,17 +6,17 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 20:11:54 by smun              #+#    #+#             */
-/*   Updated: 2021/04/11 01:52:40 by smun             ###   ########.fr       */
+/*   Updated: 2021/06/29 18:29:00 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 #include <stdlib.h>
 
-static char		*convrt(char *b, int n)
+static char		*convert(char *b, int n)
 {
 	if (n / 10)
-		b = convrt(b, n / 10);
+		b = convert(b, n / 10);
 	*b = '0' + ft_abs(n % 10);
 	return (b + 1);
 }
@@ -29,7 +29,7 @@ char			*ft_itoa(int n)
 	ft_bzero(buf, 32);
 	if (n < 0)
 		buf[0] = '-';
-	convrt(&buf[n < 0], n);
+	convert(&buf[n < 0], n);
 	if ((a = malloc(1 + ft_strlen(buf))))
 		ft_strlcpy(a, buf, 1 + ft_strlen(buf));
 	return (a);
