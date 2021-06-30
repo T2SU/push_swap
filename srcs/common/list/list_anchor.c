f6c1790c54625917a0119d40731dce4d792ac61b
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-06-30 09:15:37 +0900

    if -> while

--- srcs/common/list/list_anchor.c
+++ srcs/common/list/list_anchor.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 14:11:38 by smun              #+#    #+#             */
-/*   Updated: 2021/06/30 08:33:30 by smun             ###   ########.fr       */
+/*   Updated: 2021/06/30 09:13:40 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		list_tidy_up_anchor(t_list *list)
 {
-	if (list->anchor < 0)
+	while (list->anchor < 0)
 	{
 		if (list->length > 0)
 			list->anchor += list->length;
